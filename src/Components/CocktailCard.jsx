312b592954cb84d8b68a69d4b9bebc7df8fd3738
spmaxsp <mexx@spahn-rodgau.de>
2022-10-20 15:12:08 +0200

    Adding things to modal and loading ingrediants

--- src/Components/CocktailCard.jsx
+++ src/Components/CocktailCard.jsx
@@ -4,47 +4,126 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
+import Modal from 'react-bootstrap/Modal';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Badge from 'react-bootstrap/Badge';
+import { Container } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+
+function MyVerticallyCenteredModal(props) {
+    return (
+        <Modal {...props} size="md" aria-labelledby="contained-modal-title-vcenter" centered >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                {props.info.name}
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="p-0">
+                <img src={"http://localhost:43560/image/get/cocktail/"  + props.id} className="img-fluid w-100 m-0 p-0" /> 
+                <Container className="p-4">
+                    <h5>Recepie:</h5>
+                    <ListGroup variant="flush">
+                        {
+                            Object.keys(props.info.recepie).map((key) => (
+                                <ListGroup.Item>{props.ingrediants[key]}: {props.info.recepie[key]["amount"]}ml</ListGroup.Item>
+                            ))
+                        }
+                    </ListGroup>
+                    <Form.Label>Range</Form.Label>
+                    <Form.Range />
+                    <Form.Label>Range</Form.Label>
+                    <Form.Range />
+                </Container>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
 
 class CocktailCard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            error: null,
-            isLoaded: false,
-            items: {}
+            fetchCocktail: {
+                error: null,
+                isLoaded: false,
+                items: {}
+            },
+            fetchIngrediant: {
+                error: null,
+                isLoaded: false,
+                items: {}
+            },
+            modalShow: false,
         };
     }
 
-    fetchData() {
-        console.log("fetchData")
-        fetch("http://localhost:43560/cocktail/info/" + this.props.id)
+    fetchCocktailData(id) {
+        console.log("fetchcocktailData")
+        fetch("http://localhost:43560/cocktail/info/" + id)
             .then(res => res.json())
             .then((result) => {
                 console.log("data: " + result);
                 this.setState({
+                    fetchCocktail: {
                         isLoaded: true,
                         items: result
-                    });
-                console.log("is Loaded (fetch): " + this.state.isLoaded);
-                console.log("items:" + this.state.items)
+                    }
+                });
+                console.log("is Loaded (fetch): " + this.state.fetchCocktail.isLoaded);
+                console.log("items:" + this.state.fetchCocktail.items)
                 },
 
                 (error) => {
                     this.setState({
-                        isLoaded: true,
-                        error
+                        fetchCocktail: {
+                            isLoaded: true,
+                            error
+                        }
                     });
-                    console.log("is Loaded (fetch): " + this.state.isLoaded);
+                    console.log("is Loaded (fetch): " + this.state.fetchCocktail.isLoaded);
+                }
+            )
+    }
+
+    fetchIngrediantData() {
+        console.log("fetchcocktailData")
+        fetch("http://localhost:43560/ingrediant/list")
+            .then(res => res.json())
+            .then((result) => {
+                console.log("data: " + result);
+                this.setState({
+                    fetchIngrediant: {
+                            isLoaded: true,
+                            items: result
+                        }
+                    });
+                console.log("is Loaded (fetch): " + this.state.fetchIngrediant.isLoaded);
+                console.log("items:" + this.state.fetchIngrediant.items)
+                },
+
+                (error) => {
+                    this.setState({
+                        fetchIngrediant: {
+                            isLoaded: true,
+                            error
+                        }
+                    });
+                    console.log("is Loaded (fetch): " + this.state.fetchIngrediant.isLoaded);
                 }
             )
     }
     
     componentDidMount() {
-        this.fetchData();
+        this.fetchCocktailData(this.props.id);
+        this.fetchIngrediantData();
     } 
 
     render() {
-        const { error, isLoaded, items } = this.state;
+        const { error, isLoaded, items } = this.state.fetchCocktail;
+        const ingrediants = this.state.fetchIngrediant.items;
         if (error) {
             return( <div>Error: {error.message}</div> );
         } 
@@ -59,10 +138,28 @@ class CocktailCard extends React.Component {
                     <Card.Body>
                         <Card.Title>{items.name}</Card.Title>
                         <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
+                            <ListGroup variant="flush">
+                                {
+                                    Object.keys(items.recepie).map((key) => (
+                                        <ListGroup.Item>{ingrediants[key]}: {items.recepie[key]["amount"]}ml</ListGroup.Item>
+                                    ))
+                                }
+                            </ListGroup>
+                            <Badge pill bg="primary">
+                                Likes: {items.likes}
+                            </Badge>{' '}
                         </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <Button variant="primary" onClick={() => this.setState({modalShow: true})}>
+                            Order your "{items.name}"
+                        </Button>
+
+                        <MyVerticallyCenteredModal
+                            id={this.props.id}
+                            info={items}
+                            ingrediants={ingrediants}
+                            show={this.state.modalShow}
+                            onHide={() => this.setState({modalShow: false})}
+                        />
                     </Card.Body>
                 </Card>
             </Col>
