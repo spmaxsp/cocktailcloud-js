1d8148944b4a15557e6a7765df1ab9e73524b665
spmaxsp <mexx@spahn-rodgau.de>
2023-05-09 09:10:37 +0200

    adding ingredientsFetchHooks.js

---
+++ src/Components/api/ingredientsFetchHooks.js
@@ -0,0 +1,44 @@
+import useFetch from './fetchHook.js';
+import { useState } from 'react';
+
+const useIngredients = () => {
+    const [request, setRequest] = useState({
+        api: 'v2',
+        db: 'ingrediant',
+        action: 'list',
+        id: null,
+        value: null,
+        value2: null,
+        data: null
+    });
+
+    const refreshIngredients = () => {
+        setRequest({
+            ...request,
+            action: 'list',
+        });
+    };
+
+    const addIngredient = (name) => {
+        setRequest({
+            ...request,
+            action: 'new',
+            id: null,
+            data: { 'val': name }
+        });
+    };
+
+    const removeIngredient = (id) => {
+        setRequest({
+            ...request,
+            action: 'remove',
+            id: id,
+            data: null
+        });
+    };
+
+    const { data, loading, error } = useFetch(request);
+    return { data, loading, error, refreshIngredients, addIngredient, removeIngredient };
+};
+
+export { useIngredients };
